28a99967771b9b810bbd4058c5bbd9b4bccad013
Marc <marc.tomas@uvic.cat>
2026-01-29 15:03:37 +0100

    commit of the seqClass with the correct functions from master

--- seqClass.py
+++ seqClass.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-#Aqui estem precticant el push del github
+# Classify a sequence as DNA or RNA and optionally search for a motif
+
 import sys, re
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
-parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
-parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
+parser = ArgumentParser(description='Classify a sequence as DNA or RNA')
+parser.add_argument("-s", "--seq", type=str, required=True, help="Input sequence")
+parser.add_argument("-m", "--motif", type=str, required=False, help="Motif to search")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -13,21 +14,28 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()                 # Note we just added this line
-if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq):
-        print ('The sequence is RNA')
-    else:
-        print ('The sequence can be DNA or RNA')
+seq = args.seq.upper()   #Note we just added this line
+
+if not re.fullmatch(r"[ACGTU]+", seq):
+    print("The sequence is not DNA nor RNA (invalid characters found)")
+    sys.exit(1)
+
+contains_T = "T" in seq
+contains_U = "U" in seq
+
+if contains_T and contains_U:
+    print("Invalid sequence: contains both T and U")
+elif contains_T:
+    print("The sequence is DNA")
+elif contains_U:
+    print("The sequence is RNA")
 else:
-    print ('The sequence is not DNA nor RNA')
+    print("The sequence can be DNA or RNA")
 
 if args.motif:
-    args.motif = args.motif.upper()
-    print(f'Motif search enabled: looking for the motif "{args.motif}" in the sequence "{args.seq}"... ', end = '')
-    if re.search(args.motif, args.seq):
+    motif = args.motif.upper()
+    print(f'Motif search enabled: looking for "{motif}" in the sequence')
+    if re.search(motif, seq):
         print("I FOUND SOMETHING")
     else:
         print("NOT FOUND IN ANY PLACE")
